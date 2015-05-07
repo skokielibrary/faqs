f4d15d80ddee75cf9de0394cb119cc5edafb4c23
Brodie Austin <baustin@skokielibrary.info>
2015-05-07 11:37:59 -0500

    added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minify_css = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   gulp.src('./scss/*.scss')
       .pipe(sass())
-      .pipe(minify_css({keepBreaks:true}))
+      .pipe(minify_css())
+      .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
       .pipe(gulp.dest('./css'));
 });
 
